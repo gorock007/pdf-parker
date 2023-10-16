220a02c391cff8e528f673ba858ba1176210e1fb
Gorock Shetty <gorock397@gmail.com>
2023-10-17 09:48:52 +1100

    Added Kinde Auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
+import {LoginLink, RegisterLink} from "@kinde-oss/kinde-auth-nextjs/server"
+import { ArrowRight } from "lucide-react"
 
 const Navbar = () =>{
     return(
@@ -17,10 +19,22 @@ const Navbar = () =>{
                             <Link 
                                 href='/pricing' 
                                 className={buttonVariants({
-                                variant:"ghost",
-                                size:'sm'
-                            })}>Pricing</Link>
-                           
+                                    variant:"ghost",
+                                    size:'sm'
+                                })}>
+                                Pricing
+                            </Link>
+                            <LoginLink className={buttonVariants({
+                                variant: "ghost",
+                                size: 'sm'
+                            })}>
+                            Sign in
+                           </LoginLink>
+                            <RegisterLink className={buttonVariants({
+                                size: 'sm'
+                            })}>
+                            Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+                           </RegisterLink>
                         
                         </>
                     </div>
