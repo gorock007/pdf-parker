094a367cee076d47bd88b7e41dbe255ff1f0aaaa
Gorock Shetty <gorock397@gmail.com>
2023-11-09 07:30:04 +1100

    receiving stream on the client

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -6,6 +6,7 @@ import Messages from "./Messages"
 import { ChevronLeft, Loader2, XCircle } from "lucide-react"
 import Link from "next/link"
 import { buttonVariants } from "../ui/button"
+import { ChatContextProvider } from "./ChatContext"
 
 interface chatWrapperProps{
   fileId: string
@@ -67,12 +68,14 @@ const ChatWrapper = ({fileId}: chatWrapperProps) => {
   )
 
   return (
-    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
-      <div className="flex-1 justify-between flex flex-col mb-28">
-        <Messages />
+    <ChatContextProvider fileId={fileId}>
+      <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+        <div className="flex-1 justify-between flex flex-col mb-28">
+          <Messages />
+        </div>
+        <ChatInput/>
       </div>
-      <ChatInput/>
-    </div>
+    </ChatContextProvider>
   )
 }
 
