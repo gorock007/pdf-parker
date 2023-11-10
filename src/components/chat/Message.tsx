025544afa493459818165576336002cf8764d0b3
Gorock Shetty <gorock397@gmail.com>
2023-11-10 22:43:44 +1100

    optimistic updates for instant feedback

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -3,15 +3,17 @@ import { ExtendedMessage } from "@/types/message"
 import { Icons } from "../Icons"
 import ReactMarkdown from 'react-markdown'
 import { format } from "date-fns"
+import { forwardRef } from "react"
 
 interface MessageProps{
     message: ExtendedMessage,
     isNextMessageSamePerson: boolean
 }
 
-const Message = ({message, isNextMessageSamePerson}: MessageProps) => {
+const Message = forwardRef<HTMLDivElement, MessageProps>(({
+    message, isNextMessageSamePerson}, ref) => {
   return (
-    <div className={cn('flex items-end', {
+    <div ref={ref} className={cn('flex items-end', {
         'justify-end': message.isUserMessage
         })}>
             {/* message icon for both chat */}
@@ -59,6 +61,5 @@ const Message = ({message, isNextMessageSamePerson}: MessageProps) => {
             </div>
     </div>
   )
-}
-
+})
 export default Message
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -3,12 +3,18 @@ import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
 import { Loader2, MessageSquare } from "lucide-react"
 import Skeleton from "react-loading-skeleton"
 import Message from "./Message"
+import { useContext, useEffect, useRef } from "react"
+import { ChatContext } from "./ChatContext"
+import {useIntersection} from '@mantine/hooks'
 
 interface MessagesProps{
   fileId: string
 }
 
 const Messages = ({fileId}: MessagesProps) => {
+  //handling loading state
+  const {isLoading: isAiThinking} = useContext(ChatContext)
+
   const {data, isLoading, fetchNextPage}  = trpc.getFileMessages.useInfiniteQuery({
     fileId,
     limit: INFINITE_QUERY_LIMIT,
@@ -30,9 +36,23 @@ const Messages = ({fileId}: MessagesProps) => {
   }
 
   const combinedMessages = [
-    ...(true? [loadingMessage] :[]),
+    ...(isAiThinking? [loadingMessage] :[]),
     ...(messages?? [])
   ]
+  
+  const lastMessageRef = useRef<HTMLDivElement>(null)
+
+  const {ref, entry} = useIntersection({
+    root: lastMessageRef.current,
+    threshold:1
+  })
+
+  useEffect(() =>{
+    if (entry?.isIntersecting){
+      //if intersecting looad more messages
+      fetchNextPage()
+    }
+  }, [entry, fetchNextPage])
 
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
@@ -43,6 +63,7 @@ const Messages = ({fileId}: MessagesProps) => {
 
           if(i=== combinedMessages.length-1){
             return <Message 
+              ref = {ref}
               isNextMessageSamePerson={isNextMessageSamePerson} 
               key={message.id}
               message={message}
