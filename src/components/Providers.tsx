c695117ac54a03cf158ebad36d751358f8f4dea1
Gorock Shetty <gorock397@gmail.com>
2023-12-09 20:08:48 +1100

    Refactor: replace local url with absolute url

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,14 +4,16 @@ import { PropsWithChildren, useState } from "react"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import { trpc } from "@/app/_trpc/client"
 import { httpBatchLink } from "@trpc/client"
+import { absoluteUrl } from "@/lib/utils"
 
 const Providers = ({children}:PropsWithChildren ) => {
   const [queryClient] = useState(() => new QueryClient())
   const [trcpClient] = useState(() =>trpc.createClient({
     links: [
       httpBatchLink({
-        url:'http://localhost:3000/api/trpc',
-      })
+        // url:'http://localhost:3000/api/trpc',
+        url: absoluteUrl("/api/trpc"),
+      }),
     ]
   }))
   return(
